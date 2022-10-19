2a8bf7334e696045b30c83194514eb714269fdf1
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-10-19 16:14:07 +0300

    firebase config

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { projectAuth } from '../../firebase/config'
 
 const Signup = () => {
 	const [username, setUsername] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+
+	const  pro
 
 	const handleSubmit = (e) => {
       e.preventDefault()
